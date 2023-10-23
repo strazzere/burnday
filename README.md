8b1722330052c1f3489c911cc1ca90fd93dc5a38
Tim Strazzere <tim@strazzere.com>
2023-10-22 19:47:20 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# burnday
+
+## Source
+
+Originally parsed from a Google Sheet, though Placer county recently (2023 EOY burn season) switched to a better site, parsed from `https://itwebservices.placer.ca.gov/APCDBDI/home/`.
---
+++ index.ts
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { load } from 'cheerio';
+
+const burnDayUrl = 'https://itwebservices.placer.ca.gov/APCDBDI/home/'
+
+const westernSelector = 'zbody > div.container.body-content > div > table > tbody > tr:nth-child(1) > td:nth-child(1)';
+const expectedWestern = 'Western Placer County (West of Cisco Grove)';
+
+const dateSelector = 'body > div.container.body-content > div > table > thead > tr > th:nth-child(3)';
+const burnDataSelector = 'body > div.container.body-content > div > table > tbody > tr:nth-child(1) > td:nth-child(3)';
+
+function isToday(date: string): boolean {
+  const months: { [key: string]: number } = {
+    January: 0,
+    February: 1,
+    March: 2,
+    April: 3,
+    May: 4,
+    June: 5,
+    July: 6,
+    August: 7,
+    September: 8,
+    October: 9,
+    November: 10,
+    December: 11,
+  };
+  
+  const parts = date.split(", ");
+  const dateParts = parts[1].split(" ");
+  const year = parseInt(parts[2]);
+  const month = months[dateParts[0]];
+  const day = parseInt(dateParts[1]);
+  
+  const givenDate = new Date(year, month, day);
+  const today = new Date();
+
+  return (
+    givenDate.getDate() === today.getDate() &&
+    givenDate.getMonth() === today.getMonth() &&
+    givenDate.getFullYear() === today.getFullYear()
+  );
+}
+
+function expected(western: string | null, date: string | null): boolean {
+  if (!western || !date)
+    return false;
+
+  if (western.indexOf(expectedWestern) === -1)
+    return false;
+
+  return isToday(date);
+}
+
+function isBurnDate(info: string | undefined) {
+  if (!info)
+    return false
+
+  return !info.includes('No ')
+}
+
+axios.get(burnDayUrl)
+  .then((response) => {
+    const $ = load(response.data)
+    
+    if (!expected($(westernSelector).html(), $(dateSelector).html())) {
+      console.log(`Page was not as expected, likely need to adjust selectors.`)
+    }
+  
+    console.log(isBurnDate($(burnDataSelector).html()?.trimEnd().trimStart()))
+  })
