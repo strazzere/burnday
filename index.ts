27279507309a485a2b00383912de80f68052c7bb
Tim Strazzere <tim@strazzere.com>
2024-07-02 10:41:26 -0700

    (fix) add burn ban in effect, add linter

--- index.ts
+++ index.ts
@@ -1,113 +1,134 @@
-import axios from 'axios';
-import { load } from 'cheerio';
-import path from 'path';
-import * as fs from 'fs';
+import * as fs from "node:fs";
+import path from "node:path";
+import axios from "axios";
+import { load } from "cheerio";
 
 function overwriteToJsonFile(filePath: string, isBurnDay: boolean) {
-  const jsonData = {
-    date: new Date().toUTCString(),
-    allowedBurn: isBurnDay
-  };
+	const jsonData = {
+		date: new Date().toUTCString(),
+		allowedBurn: isBurnDay,
+	};
 
-  fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+	fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
 }
 
 function addDateToJsonFile(filePath: string, isBurnDay: boolean) {
-  const data = fs.readFileSync(filePath, 'utf-8');
-  const jsonData = JSON.parse(data);
+	const data = fs.readFileSync(filePath, "utf-8");
+	const jsonData = JSON.parse(data);
 
-  if (Array.isArray(jsonData)) {
+	if (Array.isArray(jsonData)) {
+		const today = jsonData.filter((data) => {
+			const date = new Date(data.date);
+			const today = new Date();
+			return (
+				date.getDate() === today.getDate() &&
+				date.getMonth() === today.getMonth() &&
+				date.getFullYear() === today.getFullYear()
+			);
+		});
 
-    const today = jsonData.filter((data) => {
-      const date = new Date(data.date);
-      const today = new Date();
-      return date.getDate() === today.getDate() &&
-        date.getMonth() === today.getMonth() &&
-        date.getFullYear() === today.getFullYear();
-    })
+		if (today.length > 0) {
+			return;
+		}
 
-    if (today.length > 0) {
-      return;
-    }
+		const data = {
+			date: new Date().toUTCString(),
+			allowedBurn: isBurnDay,
+		};
+		jsonData.push(data);
 
-    const data = {
-      date: new Date().toUTCString(),
-      allowedBurn: isBurnDay
-    };
-    jsonData.push(data);
+		fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+	}
+}
 
-    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+const burnDayUrl = "https://itwebservices.placer.ca.gov/APCDBDI/home/";
 
-  }
-};
+const westernTable =
+	"body > div.container.body-content > div > table > tbody > tr:nth-child(1) > td:nth-child(1)";
 
-const burnDayUrl = 'https://itwebservices.placer.ca.gov/APCDBDI/home/'
+const westernSelector = westernRowSelector(1);
+const expectedWestern = "Western Placer County (West of Cisco Grove)";
 
-const westernSelector = 'body > div.container.body-content > div > table > tbody > tr:nth-child(1) > td:nth-child(1)';
-const expectedWestern = 'Western Placer County (West of Cisco Grove)';
+const permitInfoSelector = westernRowSelector(2);
 
-const dateSelector = 'body > div.container.body-content > div > table > thead > tr > th:nth-child(4)';
-const burnDataSelector = 'body > div.container.body-content > div > table > tbody > tr:nth-child(1) > td:nth-child(4)';
+const dateSelector =
+	"body > div.container.body-content > div > table > thead > tr > th:nth-child(4)";
+const burnDataSelector = westernRowSelector(4);
+
+const burnBan = "BURN BAN IN EFFECT UNTIL FURTHER NOTICE".toLowerCase();
+
+function westernRowSelector(column: number): string {
+	return `body > div.container.body-content > div > table > tbody > tr:nth-child(1) > td:nth-child(${column})`;
+}
 
 function isToday(date: string): boolean {
-  const months: { [key: string]: number } = {
-    January: 0,
-    February: 1,
-    March: 2,
-    April: 3,
-    May: 4,
-    June: 5,
-    July: 6,
-    August: 7,
-    September: 8,
-    October: 9,
-    November: 10,
-    December: 11,
-  };
-  
-  const parts = date.split(", ");
-  const dateParts = parts[1].split(" ");
-  const year = parseInt(parts[2]);
-  const month = months[dateParts[0]];
-  const day = parseInt(dateParts[1]);
-  
-  const givenDate = new Date(year, month, day);
-  const today = new Date();
+	const months: { [key: string]: number } = {
+		January: 0,
+		February: 1,
+		March: 2,
+		April: 3,
+		May: 4,
+		June: 5,
+		July: 6,
+		August: 7,
+		September: 8,
+		October: 9,
+		November: 10,
+		December: 11,
+	};
 
-  return (
-    givenDate.getDate() === today.getDate() &&
-    givenDate.getMonth() === today.getMonth() &&
-    givenDate.getFullYear() === today.getFullYear()
-  );
+	const parts = date.split(", ");
+	const dateParts = parts[1].split(" ");
+	const year = Number.parseInt(parts[2]);
+	const month = months[dateParts[0]];
+	const day = Number.parseInt(dateParts[1]);
+
+	const givenDate = new Date(year, month, day);
+	const today = new Date();
+
+	return (
+		givenDate.getDate() === today.getDate() &&
+		givenDate.getMonth() === today.getMonth() &&
+		givenDate.getFullYear() === today.getFullYear()
+	);
 }
 
 function expected(western: string | null, date: string | null): boolean {
-  if (!western || !date)
-    return false;
+	if (!western || !date) return false;
 
-  if (western.indexOf(expectedWestern) === -1)
-    return false;
+	if (western.indexOf(expectedWestern) === -1) return false;
 
-  return isToday(date);
+	return isToday(date);
 }
 
 function isBurnDate(info: string | undefined) {
-  if (!info)
-    return false
+	if (!info) return false;
 
-  return !info.includes('No ')
+	return !info.includes("No ");
 }
 
-axios.get(burnDayUrl)
-  .then((response) => {
-    const $ = load(response.data);
-    
-    if (!expected($(westernSelector).html(), $(dateSelector).html())) {
-      throw new Error(`Page was not as expected, likely need to adjust selectors.`);
-    }
-  
-    const isBurnDay = isBurnDate($(burnDataSelector).html()?.trimEnd().trimStart());
+axios.get(burnDayUrl).then((response) => {
+	const $ = load(response.data);
 
-    addDateToJsonFile(path.join(__dirname, 'data/burnday-history.json'), isBurnDay);
-    overwriteToJsonFile(path.join(__dirname, 'data/burnday.json'), isBurnDay);
-  })
+	if ($(permitInfoSelector).html()?.toLowerCase().includes(burnBan)) {
+		addDateToJsonFile(path.join(__dirname, "data/burnday-history.json"), false);
+		overwriteToJsonFile(path.join(__dirname, "data/burnday.json"), false);
+		return;
+	}
+
+	if (!expected($(westernSelector).html(), $(dateSelector).html())) {
+		throw new Error(
+			"Page was not as expected, likely need to adjust selectors.",
+		);
+	}
+
+	const isBurnDay = isBurnDate(
+		$(burnDataSelector).html()?.trimEnd().trimStart(),
+	);
+
+	addDateToJsonFile(
+		path.join(__dirname, "data/burnday-history.json"),
+		isBurnDay,
+	);
+	overwriteToJsonFile(path.join(__dirname, "data/burnday.json"), isBurnDay);
+});
